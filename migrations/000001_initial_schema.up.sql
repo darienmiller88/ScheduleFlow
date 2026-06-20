46fc7264719faf4e72f3657a5563561fe4265811
darie <darienmiller88@yahoo.com>
2026-06-20 09:51:56 -0400

    feat: added initial sql migration file

---
+++ migrations/000001_initial_schema.up.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS specialists (
+    id            SERIAL PRIMARY KEY,
+    created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+    first_name    VARCHAR(30) NOT NULL UNIQUE,
+    last_name     VARCHAR(30) NOT NULL UNIQUE,
+    email         VARCHAR(100) NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS supervisors (
+    id            SERIAL PRIMARY KEY,
+    created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+    first_name    VARCHAR(30) NOT NULL UNIQUE,
+    last_name     VARCHAR(30) NOT NULL UNIQUE,
+    email         VARCHAR(100) NOT NULL UNIQUE,
+);
+
+CREATE TABLE IF NOT EXISTS schedules (
+    id            SERIAL PRIMARY KEY,
+    created_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at    TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+    specialist_id INTEGER NOT NULL,
+    file_path     TEXT NOT NULL,
+    send_date     TIMESTAMPTZ NOT NULL,
+
+    FOREIGN KEY (specialist_id) REFERENCES specialists(id) ON DELETE CASCADE
+);
