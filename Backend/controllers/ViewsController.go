b77a7ad4ac481b90898aadda7a45035a872d8c67
darie <darienmiller88@yahoo.com>
2026-06-21 13:25:43 -0400

    feat: added log in page

--- Backend/controllers/ViewsController.go
+++ Backend/controllers/ViewsController.go
@@ -14,17 +14,13 @@ type ViewsController struct {
 }
 
 func NewViewsController() *ViewsController {
-	partialFiles, err := filepath.Glob("./templates/partials/*.html")
-
-	if err != nil {
-		panic(err)
-	}
-
-	partialFiles = append(partialFiles, "./templates/home.html")
+	partials, _ := filepath.Glob("./templates/partials/*.html")
+	pages, _    := filepath.Glob("./templates/pages/*.html")
+	htmlFiles   := append(partials, pages...)
 
 	vc := &ViewsController{
 		Router:    chi.NewRouter(),
-		templates: template.Must(template.ParseFiles(partialFiles...)),
+		templates: template.Must(template.ParseFiles(htmlFiles...)),
 	}
 
 	vc.registerViewRoutes()
@@ -34,10 +30,19 @@ func NewViewsController() *ViewsController {
 
 func (v *ViewsController) registerViewRoutes() {
 	v.Router.Get("/home", v.homePage)
+	v.Router.Get("/login", v.loginPage)
 }
 
 func (v *ViewsController) homePage(response http.ResponseWriter, request *http.Request) {
 	err := v.templates.ExecuteTemplate(response, "home.html", nil)
+
+	if err != nil {
+		http.Error(response, "Error rendering template", http.StatusInternalServerError)
+	}
+}
+
+func (v *ViewsController) loginPage(response http.ResponseWriter, request *http.Request) {
+	err := v.templates.ExecuteTemplate(response, "login.html", nil)
 
 	if err != nil {
 		http.Error(response, "Error rendering template", http.StatusInternalServerError)
