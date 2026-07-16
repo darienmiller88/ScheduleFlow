2f2972a34a861032d58f22e97f37c811b9d52c81
darie <darienmiller88@yahoo.com>
2026-07-16 09:25:08 -0400

    feat: add sql constants for specialists

--- Backend/repositories/SpecialistRepository.go
+++ Backend/repositories/SpecialistRepository.go
@@ -1,7 +1,16 @@
 package repositories
 
-type SpecialistRepository interface {
+import(
+	"ScheduleFlow/Backend/models"
+)
 
+type SpecialistRepository interface {
+	AddSpecialist(specialist models.Specialist) models.Result[models.Specialist]
+	GetPasswordByEmail(email string) models.Result[string]
+	GetSpecialistByEmail(email string) models.Result[models.Specialist]
+	GetSpecialistById(id int) models.Result[models.Specialist]
+	UpdateSpecialist(specialist models.Specialist) models.Result[models.Specialist]
+	DeleteSpecialist(id int) models.Result[bool]
 }
 
 // Implementation of the SpecialistRepository interface using sql
@@ -11,4 +20,28 @@ type specialistRepository struct {
 
 func NewSpecialistRepository() SpecialistRepository {
 	return &specialistRepository{}
+}
+
+func (r *specialistRepository) AddSpecialist(specialist models.Specialist) models.Result[models.Specialist] {
+	return models.Result[models.Specialist]{}
+}
+
+func (r *specialistRepository) GetPasswordByEmail(email string) models.Result[string] {
+	return models.Result[string]{}
+}
+
+func (r *specialistRepository) GetSpecialistByEmail(email string) models.Result[models.Specialist] {
+	return models.Result[models.Specialist]{}
+}
+
+func (r *specialistRepository) GetSpecialistById(id int) models.Result[models.Specialist] {
+	return models.Result[models.Specialist]{}
+}	
+
+func (r *specialistRepository) UpdateSpecialist(specialist models.Specialist) models.Result[models.Specialist] {
+	return models.Result[models.Specialist]{}
+}	
+
+func (r *specialistRepository) DeleteSpecialist(id int) models.Result[bool] {
+	return models.Result[bool]{}
 }
