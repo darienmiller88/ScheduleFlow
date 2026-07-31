2b9c2ce1e845b5130c4daa935091823496f0811e
darie <darienmiller88@yahoo.com>
2026-07-31 16:42:00 -0400

    feat: turned name abbrev into a db field on struct specialist

--- Backend/models/Specialist.go
+++ Backend/models/Specialist.go
@@ -37,18 +37,18 @@ var (
 )
 
 type Specialist struct {
-	ID        int       `db:"id"`
-	CreatedAt time.Time `db:"created_at"`
-	UpdatedAt time.Time `db:"updated_at"`
+	ID               int       `db:"id"`
+	CreatedAt        time.Time `db:"created_at"`
+	UpdatedAt        time.Time `db:"updated_at"`
 
 	//Relevant fields for the Specialist model
-	FirstName string `db:"first_name"`
-	LastName  string `db:"last_name"`
-	Email     string `db:"email"`
-	Password  string `db:"password_hash"`
+	FirstName        string `db:"first_name"`
+	LastName         string `db:"last_name"`
+	Email            string `db:"email"`
+	Password         string `db:"password_hash"`
 
-	//Not a db field, will hold the first letter of the first and last name, ex -> (D)arien (M)iller = DM
-	PlayerNameAbbrev string `db:"name_abbrev"`
+	//will hold the first letter of the first and last name, ex -> (D)arien (M)iller = DM
+	PlayerNameAbbrev string `db:"name_abbrev"`	
 }
 
 func (s *Specialist) Validate() error {
