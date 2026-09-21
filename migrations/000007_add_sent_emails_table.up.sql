a5e4b340af53dfabfca25d8a3a35a0c32eaea77a
darie <darienmiller88@yahoo.com>
2026-09-21 12:11:20 -0400

    fix: modified table to fix foreign key reference

--- migrations/000007_add_sent_emails_table.up.sql
+++ migrations/000007_add_sent_emails_table.up.sql
@@ -3,8 +3,9 @@ CREATE TABLE sent_emails (
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 
-    specialist_id INT NOT NULL REFERENCES specialists(specialist_id),
     file_path     text NOT NULL,       -- snapshot of what was actually sent
+    specialist_id INT NOT NULL,
+    FOREIGN KEY (specialist_id) REFERENCES specialists(id)
 );
 
 CREATE INDEX idx_sent_emails_specialist_id ON sent_emails(specialist_id);
